8beb76d6b9e3aa9bffee820984896ba4e4340ee6
Deanne Anorico <20-78556@g.batstate-u.edu.ph>
2023-09-03 23:00:05 +0800

    login.html lang

--- forms.php
+++ forms.php
@@ -19,15 +19,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $activity_title = $_POST["activityName"];
     $college = $_POST["department"];
     $program = $_POST["program"];
-    $partner = $_POST["partner"];
-    $start_date = $_POST["start_date"];
-    $end_date = $_POST["end_date"];
-    // $partner_name = $_POST["partner_name"]; partner_name  '$partner_name', 
-    // $roles = $_POST["roles"]; '$roles', roles,
-    $rationale = $_POST["rationale"];
-    $objectives = $_POST["objectives"];
-    $budget_source = $_POST["budget"];
-    
+    $partner_name = $_POST["partner"];
+
     // Check if the "partnerType" field is set and not empty
     if (isset($_POST["partnerType"]) && !empty($_POST["partnerType"])) {
         $partner_type = $_POST["partnerType"];
@@ -36,14 +29,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $partner_type = ""; // You can set a default value or handle the error as needed
     }
 
+    $start_date = $_POST["start_date"];
+    $end_date = $_POST["end_date"];
+    $rationale = $_POST["rationale"];
+    $objectives = $_POST["objectives"];
+    $budget_source = $_POST["budget"];
+    
     // Insert the data into the database
-    $sql = "INSERT INTO activityform (activity_title, college, program, partner_name, partner) VALUES (?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO activityform (activity_title, college, program, partner_name, partner, start_date, end_date, rationale, objectives, budget_source) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
     // Prepare the SQL statement using a prepared statement
     $stmt = $conn->prepare($sql);
 
     // Bind parameters to the prepared statement
-    $stmt->bind_param("sssss", $activity_title, $college, $program, $partner_name, $partner_type);
+    $stmt->bind_param("ssssssssss", $activity_title, $college, $program, $partner_name, $partner_type, $start_date, $end_date, $rationale, $objectives, $budget_source);
 
     if ($stmt->execute()) {
         $formSubmitted = true; // Set the flag to true if the form submission was successful
