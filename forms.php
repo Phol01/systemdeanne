4ddbfc0aad750ab6a7838756f652c82a83ac629c
unknown <joelapuk11@gmail.com>
2023-09-03 21:17:53 +0700

    Yung sa radio btn

--- forms.php
+++ forms.php
@@ -19,24 +19,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $activity_title = $_POST["activityName"];
     $college = $_POST["department"];
     $program = $_POST["program"];
-    $partner = $_POST["partner"];
-    $duration = $_POST["date"];
+    $partner_name = $_POST["partner"];
+    
+    // Check if the "partnerType" field is set and not empty
+    if (isset($_POST["partnerType"]) && !empty($_POST["partnerType"])) {
+        $partner_type = $_POST["partnerType"];
+    } else {
+        // Handle the case where "partnerType" is not set or empty
+        $partner_type = ""; // You can set a default value or handle the error as needed
+    }
 
     // Insert the data into the database
-    $sql = "INSERT INTO activityform (activity_title, college, program, partner, duration) VALUES ('$activity_title', '$college', '$program', '$partner', '$duration')";
+    $sql = "INSERT INTO activityform (activity_title, college, program, partner_name, partner) VALUES (?, ?, ?, ?, ?)";
 
-    if ($conn->query($sql) === TRUE) {
+    // Prepare the SQL statement using a prepared statement
+    $stmt = $conn->prepare($sql);
+
+    // Bind parameters to the prepared statement
+    $stmt->bind_param("sssss", $activity_title, $college, $program, $partner_name, $partner_type);
+
+    if ($stmt->execute()) {
         $formSubmitted = true; // Set the flag to true if the form submission was successful
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Error: " . $stmt->error;
     }
-}
 
-$conn->close();
+    // Close the prepared statement
+    $stmt->close();
+}
 
 // Check if the form was successfully submitted and then redirect to ui-formsPreview.php
 if ($formSubmitted) {
     header("Location: ui-formsPreview.php");
     exit(); // Ensure that no more content is sent after the redirection
 }
+
+$conn->close();
 ?>
